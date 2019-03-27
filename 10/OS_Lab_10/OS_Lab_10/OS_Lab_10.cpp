a7ed2547727387570f96bbf2ff67e818eaff2d9e
alyona0stepashka <paspaspas01@yandex,ru>
2019-03-27 11:08:47 +0300

    10

---
+++ 10/OS_Lab_10/OS_Lab_10/OS_Lab_10.cpp
@@ -0,0 +1,159 @@
+﻿// OS_Lab_10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "string.h"
+#include <ole2.h>
+#include<stdio.h>
+
+HRESULT AutoWrap(int autoType, VARIANT *pvResult, IDispatch *pDisp,
+	LPOLESTR ptName, int cArgs...)
+{
+	// Begin variable-argument list...
+	va_list marker;
+	va_start(marker, cArgs); //формируем список аргументов, передаваемых в cArgs
+
+	if (!pDisp) {
+		MessageBox(NULL, TEXT("NULL IDispatch passed to AutoWrap()"),
+			TEXT("Error"), 0x10010);
+		_exit(0);
+	}
+	// pDisp – это объект, на котoром выполняется метод, напр. document
+	// Variables used...
+	DISPPARAMS dp = { NULL, NULL, 0, 0 }; //структура определяет параметры,
+	// передаваемые в метод
+	DISPID dispidNamed = DISPID_PROPERTYPUT;
+	DISPID dispID;
+	HRESULT hr;
+	char buf[200];
+	char szName[200];
+	// Convert down to ANSI
+	WideCharToMultiByte(CP_ACP, 0, ptName, -1, szName, 256, NULL, NULL);
+	// Get DISPID for name passed...
+	hr = pDisp->GetIDsOfNames(IID_NULL, &ptName, 1, LOCALE_USER_DEFAULT,
+		&dispID);
+	if (FAILED(hr)) {
+		sprintf(buf,
+			"IDispatch::GetIDsOfNames(\"%s\") failed w/err0x%08lx",
+			szName, hr);
+		MessageBox(NULL, TEXT("Click"), TEXT("AutoWrap()"), 0x10010);
+		_exit(0);
+		return hr;
+	}
+	// Allocate memory for arguments...
+	VARIANT *pArgs = new VARIANT[cArgs + 1];
+	// Extract arguments...
+	for (int i = 0; i < cArgs; i++) {
+		pArgs[i] = va_arg(marker, VARIANT);
+	}
+	// Build DISPPARAMS
+	dp.cArgs = cArgs;
+	dp.rgvarg = pArgs;
+	// Handle special-case for property-puts!
+	if (autoType & DISPATCH_PROPERTYPUT) {
+		dp.cNamedArgs = 1;
+		dp.rgdispidNamedArgs = &dispidNamed;
+	}
+	// Make the call!
+	hr = pDisp->Invoke(dispID, IID_NULL, LOCALE_SYSTEM_DEFAULT, autoType,
+		&dp, pvResult, NULL, NULL);
+	if (FAILED(hr)) {
+		sprintf(buf,
+			"IDispatch::Invoke(\"%s\"=%08lx) failed w/err 0x%08lx",
+			szName, dispID, hr);
+		MessageBox(NULL, TEXT("Click Me"), TEXT("AutoWrap()"), 0x10010);
+		_exit(0);
+		return hr;
+	}
+	// End variable-argument section...
+	va_end(marker);
+	delete[] pArgs;
+	return hr;
+}
+
+
+int main(int argc, char* argv[])
+{
+	// Initialize COM for this thread...
+	CoInitialize(NULL);
+
+	// Get CLSID for Word.Application...
+	CLSID clsid;
+	HRESULT hr = CLSIDFromProgID(L"Word.Application", &clsid);
+	if (FAILED(hr)) {
+		::MessageBox(NULL, TEXT("CLSIDFromProgID() failed"), TEXT("Error"),
+			0x10010);
+		return -1;
+	}
+
+	// Start Word and get IDispatch...
+	IDispatch *pWordApp;
+	hr = CoCreateInstance(clsid, NULL, CLSCTX_LOCAL_SERVER,
+		IID_IDispatch, (void **)&pWordApp);
+	if (FAILED(hr)) {
+		::MessageBox(NULL, TEXT("Word not registered properly"),
+			TEXT("Error"), 0x10010);
+		return -2;
+	}
+	// Make Word visible
+	{
+		VARIANT x;
+		x.vt = VT_I4;
+		x.lVal = 1;
+		AutoWrap(DISPATCH_PROPERTYPUT, NULL, pWordApp, (LPOLESTR)L"Visible", 1,
+			x);
+	}
+	// Get Documents collection
+	IDispatch *pDocs;
+	{
+		VARIANT result;
+		VariantInit(&result);
+		AutoWrap(DISPATCH_PROPERTYGET, &result, pWordApp, (LPOLESTR)L"Documents",
+			0);
+
+		pDocs = result.pdispVal;
+	}
+	// Call Documents.Open() to open F:\Doc1.doc
+	IDispatch *pDoc;
+	{
+		VARIANT result;
+		VariantInit(&result);
+		VARIANT x;
+		x.vt = VT_BSTR;
+		x.bstrVal = ::SysAllocString(L"D:\\Doc1.docx");
+
+		AutoWrap(DISPATCH_METHOD, &result, pDocs, (LPOLESTR)L"Open", 1, x);
+		pDoc = result.pdispVal;
+		SysFreeString(x.bstrVal);
+	}
+	IDispatch *pFont;
+	{
+		VARIANT result;
+		VariantInit(&result);
+		AutoWrap(DISPATCH_PROPERTYGET, &result,
+			pDoc, (LPOLESTR)L"Font", 56);
+		pFont = result.pdispVal;
+	}
+	::MessageBox(NULL,
+		TEXT("Click Me"),
+		TEXT("word doc"), 0x10000);
+	pDoc->Release();
+	pDocs->Release();
+	pWordApp->Release();
+	// Uninitialize COM for this thread...
+	CoUninitialize();
+
+	return 0;
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
